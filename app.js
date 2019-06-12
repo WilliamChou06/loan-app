63dfb724cdea9904929ad98fae3fa7e48d0e3280
William Chou <williamchoudev@gmail.com>
2019-06-11 23:51:13 -0400

    Add loan and information endpoints functionality

--- app.js
+++ app.js
@@ -1,10 +1,39 @@
 const express = require('express');
 const database = require('./utils/database');
-const db = database();
+const bodyParser = require('body-parser');
 
+const db = database();
 const app = express();
 
 const PORT = process.env.PORT || 4000;
+
+// Bodyparser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+app.post('/loan', async (req, res) => {
+  try {
+    const findEmailRes = await db.findByEmail(req.body.email);
+    if (findEmailRes) {
+      return res.send(
+        await db.setByEmail(req.body.email, findEmailRes + req.body.amount)
+      );
+    }
+    return res.send(await db.setByEmail(req.body.email, req.body.amount));
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post('/information', async (req, res) => {
+  try {
+    const userEmail = await db.findByEmail(req.body.email);
+    res.send(userEmail);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server has started on port ${PORT}`);
--- utils/database.js
+++ utils/database.js
@@ -1,4 +1,5 @@
 module.exports = (persistence = {}) => {
+  let loanLimit = 1000;
   const findByEmail = async email =>
     await new Promise(resolve => resolve(persistence[email]));
   const setByEmail = async (email, amount) =>
@@ -6,6 +7,7 @@ module.exports = (persistence = {}) => {
       persistence[email] = amount;
       resolve(persistence);
     });
+
   return {
     findByEmail,
     setByEmail,
